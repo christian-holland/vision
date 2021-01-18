6ef9a8cecc889ad3e79082d909681bad6042990c
Christian Holland <christian.holland@logivations.com>
2021-01-18 15:12:05 +0100

    added visual inference for keypoints

--- references/detection/inference.py
+++ references/detection/inference.py
@@ -3,17 +3,31 @@ from PIL import Image
 import torch
 from train import get_transform
 import torchvision
-from inference_utils import draw_coco_box
+from inference_utils import draw_coco_box, draw_kps
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-source_dir = "/home/logi/datasets/forklift_coco/val2017"
-target_dir = "output"
+
+IS_KEYPOINT_NET = True
+# source_dir = "/home/logi/datasets/forklift_coco/val2017"
+source_dir = "/home/logi/datasets/2020_1222_brummer_dec_color/JPEGImages_Segmentations"
+target_dir = "output_keypoints_dect"
+
 allowed_types = [".png", ".jpg", ".jpeg"]
-model_type = "fasterrcnn_resnet50_fpn"
+if IS_KEYPOINT_NET:
+    model_type = "keypointrcnn_resnet50_fpn"
+    load_path = "/home/logi/repos/forklift_rcnn/vision/references/detection/model.pth"
+    kwargs = {"num_keypoints":6}
+    num_classes = 2
+else:
+    model_type = "fasterrcnn_resnet50_fpn"
+    load_path = "/home/logi/repos/forklift_rcnn/vision/references/detection/model_49.pth"
+    num_classes = 3
+    kwargs = {}
+
 score_th = 0.9
-load_path = "/home/logi/repos/forklift_rcnn/vision/references/detection/model_49.pth"
+
 
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
@@ -22,7 +36,7 @@ file_names = [imname for imname in os.listdir(source_dir)
               if any([imname.lower().endswith(dtype) for dtype in allowed_types])]
 
 transform = get_transform(train=False)
-model = torchvision.models.detection.__dict__[model_type](num_classes=3, pretrained=False)
+model = torchvision.models.detection.__dict__[model_type](num_classes=num_classes, pretrained=False, **kwargs)
 checkpoint = torch.load(load_path, map_location='cpu')
 model.load_state_dict(checkpoint['model'])
 
@@ -31,6 +45,7 @@ model.eval()
 
 for image_name in file_names:
     image_path = os.path.join(source_dir, image_name)
+    print("imname", image_name)
     image = Image.open(image_path)
     im = image.copy()
     im, _ = transform(im, None)
@@ -38,14 +53,22 @@ for image_name in file_names:
     im = im.cuda()
     outputs = model(im)[0]
     prop_inds = torch.where(outputs["scores"] > score_th)
+    print(prop_inds)
     scores =outputs["scores"][prop_inds]
     boxes = outputs["boxes"][prop_inds]
     labels = outputs["labels"][prop_inds]
+    if IS_KEYPOINT_NET:
+        keypoints = outputs["keypoints"][prop_inds][:, :, :2]
+        kp_scores = outputs["keypoints_scores"][prop_inds]
     image = cv2.imread(image_path)
     for i in range(len(scores)):
         label = labels[i]
         box = boxes[i]
         image = draw_coco_box(image, box, str(label))
+        if IS_KEYPOINT_NET:
+            image = draw_kps(image, keypoints[i], kp_scores[i])
+    # if len(prop_inds[0]):
+    #     cv2.imwrite(os.path.join(target_dir, image_name), image)
     plt.imshow(image[:,:,::-1])
     plt.show()
 
--- references/detection/inference_utils.py
+++ references/detection/inference_utils.py
@@ -1,4 +1,8 @@
 import cv2
+import numpy as np
+
+kp_map = ["RL", "RR", "FL", "FR", "LL", "LR"]
+min_score = 15
 
 def name_to_color(name):
     color = abs(hash(name))
@@ -10,3 +14,37 @@ def draw_coco_box(image, bb, instance_type):
     args = (image, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), color, 2)
     image = cv2.rectangle(*args)
     return image
+
+def draw_kps(image, keypoints, scores):
+    keypoints = keypoints.long()
+    keypoints = [tuple(list(kps)) for kps in keypoints]
+    print("scores", [float(score) for score in scores])
+    for kp, score, text in zip(keypoints, scores, kp_map):
+        if score > min_score:
+            image = cv2.circle(image, kp , radius=0, color=(0 , 0, 255), thickness=7)
+            image = write(image, kp, text)
+    return image
+
+
+def write(image, pos, text):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    fontColor = (255, 255, 255)
+    lineType = 2
+    cv2.putText(image, text,
+                pos,
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+    return image
+
+
+if __name__=="__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    image = cv2.imread("/home/logi/datasets/forklift_coco/train2017/img_2020_12_22_00_03_05t107331.jpg")
+
+    image = draw_kps(image, [[100, 100]], [0.5])
+    plt.imshow(image[:,:,::-1])
+    plt.show()
